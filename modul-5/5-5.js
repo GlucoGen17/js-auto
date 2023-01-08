2ae1b541e5e31896ef84c7ddd77e5880c35111c3
Bohdan Mazur <104269738+GlucoGen17@users.noreply.github.com>
2023-01-08 16:26:35 +0200

    Update 5-5.js

---
+++ modul-5/5-5.js
@@ -0,0 +1,19 @@
+const ancestor = {
+    name: "Paul",
+    age: 83,
+    surname: "Dawson",
+    heritage: "Irish",
+  };
+  // Change code below this line
+  
+  const parent = Object.create(child);
+  parent.name = "Stacey";
+  parent.surname = "Moore";
+  parent.age = 54;
+  
+  const child = Object.create(parent);
+  child.name = "Jason";
+  child.age = 27;
+  
+  // Change code above this line
+  
